ef7d7aa4c8b61357f16bfe76a5399836f82784eb
Diksha Shukla <60606998+dikshashukla455@users.noreply.github.com>
2022-03-17 18:13:48 +0530

    Delete src directory

--- src/components/Profile.test.js
+++
@@ -1,8 +0,0 @@
-import {render, screen} from '@testing-library/react';
-import Profile from './Profile';
-
-
-test("on displaying my profile", () => {
-	render(<Profile />);
-	screen.debug();
-});
--- src/components/Repo.test.js
+++
@@ -1,8 +0,0 @@
-import {render, screen} from '@testing-library/react';
-import Repos from './Repos';
-
-
-test("on displaying the user's repositories", () => {
-	render(<Repos />);
-	screen.debug();
-});
